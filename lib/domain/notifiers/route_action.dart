4f8c055952e401763f4486e3950243ec38bde49d
Hrvoje Čukman <hrvoje.cukman@q.agency>
2022-09-14 09:10:05 +0200

    Added docs to route_action.dart

--- lib/domain/notifiers/route_action.dart
+++ lib/domain/notifiers/route_action.dart
@@ -3,18 +3,26 @@ import 'package:reusability/common/domain/router/beamer_delegate_provider.dart';
 
 abstract class RouteAction {
   final String routeName;
+
   RouteAction([this.routeName = '']);
 
   void execute(Reader reader);
 }
 
+///Replace it with the package action which is used in the app
+///Beamer: reader(beamerDelegateProvider)?.beamToNamed(routeName);
+///Auto route: reader(appRouterProvider)?.pushNamed(routeName);
 class RouteActionPushNamed extends RouteAction {
   RouteActionPushNamed(super.routeName);
 
   @override
-  void execute(Reader reader) => reader(beamerDelegateProvider)?.beamToNamed(routeName);
+  void execute(Reader reader) =>
+      reader(beamerDelegateProvider)?.beamToNamed(routeName);
 }
 
+///Replace it with the package action which is used in the app
+///Beamer: reader(beamerDelegateProvider)?.beamBack();
+///Auto route: reader(appRouterProvider)?.pop();
 class RouteActionPop extends RouteAction {
   @override
   void execute(Reader reader) => reader(beamerDelegateProvider)?.beamBack();
@@ -23,6 +31,10 @@ class RouteActionPop extends RouteAction {
 class RouteActionPushReplacement extends RouteAction {
   RouteActionPushReplacement(super.routeName);
 
+  ///Replace it with the package action which is used in the app
+  ///Beamer: reader(beamerDelegateProvider)?.beamToReplacementNamed(routeName);
+  ///Auto route: reader(appRouterProvider)?.pushNamed(routeName);
   @override
-  void execute(Reader reader) => reader(beamerDelegateProvider)?.beamToReplacementNamed(routeName);
+  void execute(Reader reader) =>
+      reader(beamerDelegateProvider)?.beamToReplacementNamed(routeName);
 }
