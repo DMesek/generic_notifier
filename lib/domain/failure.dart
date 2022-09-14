ec1055abcdcdc55e66be024c456f5f18e8c7d738
Ivan Celija <ivan.celija@q.agency>
2022-09-14 11:43:44 +0200

    Added dartdoc for failure.dart and execute method in base_state_notifier.dart

--- lib/domain/failure.dart
+++ lib/domain/failure.dart
@@ -1,11 +1,22 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/widgets.dart';
+import 'package:reusability/presentation/common/base_state_notifier.dart';
 
+/// Failure class that represents some kind of error that occurs in the app and being passed to UI
 class Failure extends Equatable {
+  /// title of the Failure which can be shown to user
   final String title;
+
+  /// bool that says if this Failure is a critical one or not and according to it can be displayed to user in a different manner
   final bool isCritical;
+
+  /// error that can be caught by some try catch bloc, useful for debugging
   final dynamic error;
+
+  /// stackTrace of the [error] that can be caught by some try catch bloc, also useful for debugging
   final StackTrace? stackTrace;
+
+  /// uniqueKey set by [BaseStateNotifier.execute] method to trigger [globalFailureProvider] each time
   final UniqueKey? uniqueKey;
 
   const Failure._({
@@ -16,6 +27,7 @@ class Failure extends Equatable {
     this.uniqueKey,
   });
 
+  /// Generic Failure constructor with default title if not provided
   factory Failure.generic({
     String? title,
     bool isCritical = false,
