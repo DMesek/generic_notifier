22d205890f66caa516a3dd621ae56e972735ff98
Ivan Celija <ivan.celija@q.agency>
2022-09-16 11:15:59 +0200

    Moved WidgetRefExtensions to BaseWidget

--- lib/presentation/common/base_widget.dart
+++ lib/presentation/common/base_widget.dart
@@ -1,7 +1,13 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:reusability/common/domain/utils/widget_ref_extension.dart';
+import 'package:reusability/common/domain/router/base_router_provider.dart';
+import 'package:reusability/domain/failure.dart';
+import 'package:reusability/domain/notifiers/global_failure_provider.dart';
 import 'package:reusability/domain/notifiers/global_loading_provider.dart';
+import 'package:reusability/domain/notifiers/navigation_provider.dart';
+import 'package:reusability/domain/notifiers/route_action.dart';
 import 'package:reusability/presentation/common/base_loading_indicator.dart';
 
 class BaseWidget extends ConsumerWidget {
@@ -22,6 +28,26 @@ class BaseWidget extends ConsumerWidget {
         child,
         if (showLoading) const BaseLoadingIndicator(),
       ],
+    );
+  }
+}
+
+extension _WidgetRefExtensions on WidgetRef {
+  void globalFailureListener(BuildContext _) {
+    listen<Failure?>(globalFailureProvider, (_, failure) {
+      if (failure == null) return;
+      //Show global error
+      log('''showing ${failure.isCritical ? '' : 'non-'}critical failure with title ${failure.title},
+          error: ${failure.error},
+          stackTrace: ${failure.stackTrace}
+      ''');
+    });
+  }
+
+  void globalNavigationListener() {
+    listen<RouteAction?>(
+      globalNavigationProvider,
+      (_, state) => state?.execute(read(baseRouterProvider)),
     );
   }
 }
